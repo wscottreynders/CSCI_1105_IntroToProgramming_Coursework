59331a0319271ae03b92aebea1a5dfd5976ca37e
wscottreynders <56454129+wscottreynders@users.noreply.github.com>
2019-12-16 10:41:05 -0700

    Chapter 1 Files
    Added Chapter 1 files to folder

---
+++ Cahpter 1/Area and perimeter of a rectangle.java
@@ -0,0 +1,12 @@
+/* Author: Scott Reynders
+	Date: 12/16/19
+	
+	Creating a program to display the area and perimeter of a rectangle
+*/
+
+class Untitled {
+	public static void main(String[] args) {
+		System.out.println(4.5 * 7.9);
+		System.out.println(4.5 +4.5 + 7.9 + 7.9);
+	}
+}
---
+++ Cahpter 1/Solve 2 * 2 Linear equation.java
@@ -0,0 +1,14 @@
+/* Author: Scott Reynders
+	Date: 12/16/19
+	
+	writing a program for solving a 2 * 2 lineaer equations
+*/
+
+class Untitled {
+	public static void main(String[] args) {
+		System.out.println((44.5 * .55 - 50.2 * 5.9) / (3.4 * .55 - 50.2 * 2.1));
+		
+		System.out.println((3.4 * 5.9 - 44.5 * 2.1) / (3.4 * .55 - 50.2 * 2.1));
+		
+	}
+}
