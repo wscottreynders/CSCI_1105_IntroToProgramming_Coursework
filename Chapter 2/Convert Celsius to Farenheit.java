fa968731c678eb93615e8a0931e6d5f9777ff879
wscottreynders <56454129+wscottreynders@users.noreply.github.com>
2019-12-16 11:42:47 -0700

    Convert Celsius to Fahrenheit
    adding for safety

---
+++ Chapter 2/Convert Celsius to Farenheit.java
@@ -0,0 +1,21 @@
+/* Author: Scott Reynders
+	Date: 12/16/19
+	
+	Writing a program that converst Celsius to Fahrenheit
+*/
+
+class Untitled {
+	public static void main(String[] args) {
+		
+		
+		double Celsius;
+		
+		Celsius = 43;
+		
+	
+		System.out.println(Celsius + " in degrees Celsius " + "is");
+		
+		System.out.println((9 * Celsius / 5) + 32 + " in degrees Fahrenheit ");
+		
+	}
+}
