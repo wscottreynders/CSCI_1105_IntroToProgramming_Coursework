7100f6ba260546c17616a4b6dab63d8d2f85e1eb
wscottreynders <56454129+wscottreynders@users.noreply.github.com>
2019-12-17 12:48:47 -0700

    Add files via upload

---
+++ Chapter 3/SolveTwobyTwoLinearEquation.java
@@ -0,0 +1,44 @@
+/* Author: Scott Reynders
+	Date: 12/17/19
+	
+	Creating a program that solves 2 * 2 linear equations when a user enters the values in the equation
+*/
+
+import java.util.Scanner;
+
+class  SolveTwobyTwoLinearEquation{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+			System.out.print("Enter a: ");
+			double a = input.nextDouble();
+			
+			System.out.print("Enter b: ");
+			double b = input.nextDouble();
+			
+			System.out.print("Enter c: ");
+			double c = input.nextDouble();
+			
+			System.out.print("Enter d: ");
+			double d = input.nextDouble();
+			
+			System.out.print("Enter e: ");
+			double e = input.nextDouble();
+			
+			System.out.print("Enter f: ");
+			double f = input.nextDouble();
+			
+		
+		double x = ((e * d - b * f) / (a * d - b * c));
+		double y = ((a * f - e * c) / (a * d - b * c));
+		double xOne = a * d - b * c;
+		double yOne = a * d - b * c;
+		
+		if (xOne == 0) {
+			System.out.println(" Cannot divide by zero ");
+		}
+		
+			System.out.println("x = " + x);
+			System.out.println("y = " + y);
+	}
+	
+}
