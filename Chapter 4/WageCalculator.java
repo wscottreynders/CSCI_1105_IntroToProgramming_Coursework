c5f57db4ab17a25e42db40155ab5192ccd33c6d2
wscottreynders <56454129+wscottreynders@users.noreply.github.com>
2020-01-06 10:44:04 -0700

    Add files via upload

--- Chapter 4/WageCalculator.java
+++ Chapter 4/WageCalculator.java
@@ -8,26 +8,33 @@ import java.util.Scanner;
 
 class WageCalculator {
 
+// This is were the user inputs the information about the employee.
     public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
 
 	System.out.print("Enter employee's name: ");
 	String employeeName = input.nextLine();
+	
 	System.out.print("Enter number of hours worked: ");
-	double hoursWorked = input.nextFloat();
+	double hoursWorked = input.nextDouble();
+	
 	System.out.print("Enter hourly pay rate: $");
 	double hourlyPayRate = input.nextDouble();
+	
 	System.out.print("Enter federal tax withholding rate: ");
 	double federalTax = input.nextDouble();
+	
 	System.out.print("Enter state tax withholding rate: ");
 	double stateTax = input.nextDouble();
 
+// These are the equations for calculating the final values using the user inputted values.
 	double amountEarned = hourlyPayRate * hoursWorked;
 	double amountToFeds = amountEarned * federalTax;
 	double amountToState = amountEarned * stateTax;
 	double totalDeduction = amountToFeds + amountToState;
 	double netPay = amountEarned - amountToFeds - amountToState;
 
+// This is were the the calculated values are displayed for the user.
 	System.out.println("Employee Name: " + employeeName);
 	System.out.println("Hours Worked: " + hoursWorked);
 	System.out.println("Pay Rate: " + "$" + hourlyPayRate);
